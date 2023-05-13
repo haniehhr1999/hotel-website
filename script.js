4bedfa8e609c1fc59847e85f4fe45a38509974a6
hanieh <haniehrahim78@gmail.com>
2023-05-13 00:19:57 -0700

    creating part3

--- script.js
+++ script.js
@@ -98,3 +98,115 @@ box5.addEventListener('mouseleave' , function(){
     desc5.style.height = '85px'
     desc5.style.backgroundColor = 'transparent'
 })
+// for reservation ===============================================================================================
+var inside1 = document.querySelector('.inside1') //for adults
+var inside2 = document.querySelector('.inside2') //for children
+var textSelect2 = document.querySelector('#textSelect2') //for adults text
+var textSelect1 = document.querySelector('#textSelect1') //for tarikh text
+
+
+// for adult ============================
+var adult = document.querySelector('#adult')
+var flag = true
+adult.addEventListener('click' , function(){
+    var menuAdult = document.querySelector('.menu-adult')
+
+    if (flag == true) {
+        menuAdult.style.height = '200px'
+        menuAdult.style.padding = '16px'
+        flag = false
+    }else if (flag == false){
+        menuAdult.style.height = '0'
+        menuAdult.style.padding = '0'
+        flag = true
+    }
+})
+
+var plusAdult = document.querySelector('.item3-up')
+var i = 0
+plusAdult.addEventListener('click' , function(){
+    i++
+    inside1.value = i 
+    textSelect2.innerText = `${inside1.value} Adults, ${inside2.value} Children`
+})
+var minusAdult = document.querySelector('.item1-up')
+minusAdult.addEventListener('click' , function(){
+    i--
+    inside1.value -= 1
+    textSelect2.innerText = `${inside1.value} Adults, ${inside2.value} Children`
+})
+
+// for children ==========================
+var plusChildren = document.querySelector('.item3-down')
+var j = 0
+plusChildren.addEventListener('click' , function(){
+    j++
+    inside2.value = j 
+    textSelect2.innerText = `${inside1.value} Adults, ${inside2.value} Children`
+})
+var minusChildren = document.querySelector('.item1-down')
+minusChildren.addEventListener('click' , function(){
+    j--
+    inside2.value -= 1
+    textSelect2.innerText = `${inside1.value} Adults, ${inside2.value} Children`
+})
+
+// for tables ==========================
+var td = document.querySelectorAll('td')
+td.forEach(element => {
+    var flag = true
+    element.addEventListener('click' , function(){
+        if (flag == true) {
+            element.style.backgroundColor = '#a7856a'
+            flag = false
+        } else if(flag == false) {
+            element.style.backgroundColor = 'transparent'
+            flag = true
+        }
+    })
+});
+
+
+// for calendar ===================================================================================================
+var stay = document.querySelector('.stay')
+stay.innerHTML = ''
+
+var main = document.querySelector('.main-con')
+var close = document.querySelector('.close')
+close.addEventListener('click' , function(){
+    main.style.height = '0'
+    main.style.padding = '0'
+    main.style.overflow = 'hidden'
+})
+
+var calendar = document.querySelector('#tarikh') //for tarikh
+calendar.addEventListener('click' , function(){
+
+    if (flag == true) {
+        main.style.height = '280px'
+        main.style.paddingTop = '20px'
+        main.style.paddingBottom = '20px'
+        flag = false
+    }else if (flag == false){
+        main.style.height = '0'
+        main.style.padding = '0'
+        flag = true
+    }
+})
+
+
+var txtPart1 = document.querySelector('.txt-part1')
+var txtPart2 = document.querySelector('.txt-part2')
+var maay = document.querySelectorAll('.maay')
+maay.forEach(element1 => {
+    element1.addEventListener('click' , function(){
+        txtPart1.innerHTML = `${element1.innerHTML} May - `
+    })
+});
+
+var Juune = document.querySelectorAll('.Juune')
+Juune.forEach(element2 => {
+    element2.addEventListener('click' , function(){
+        txtPart2.innerHTML = `${element2.innerHTML} June`
+    })
+});
